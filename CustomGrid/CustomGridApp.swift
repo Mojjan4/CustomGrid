d558bab5895821101b254e59fdc1a5b9fa39d630
Playground <christopher@playground.tv>
2021-06-19 12:00:17 +0200

    Initial Commit

---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Christopher on 2021-06-19.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
